708af45c16f434911fae0e5b53eef0366f601f31
亦杨 <zhibing.yzb@alibaba-inc.com>
2015-12-22 21:54:34 +0800

    增加math学习记录

---
+++ study-javascript/math/README.md
@@ -0,0 +1,16 @@
+## Math
+
+与其他不同，Math不是构造器。所有提供的方法，都是静态方法。
+
+* 常用的几个
+    * Math.abs() 绝对值
+    * Math.round() 四舍五入
+    * Math.ceil() 向上取整
+    * Math.floor() 向下取整
+    * Math.trunc() 取整，舍去小数点
+    * Math.max() 取最大值
+    * Math.min() 取最小值
+    * Math.random() 0-1的随机数
+    * Math.pow() 取冥次
+    * Math.sqrt() 开根号
+    
---
+++ study-javascript/math/readme.js
@@ -0,0 +1,15 @@
+'use strict';
+
+Math.abs(-1); // 1
+Math.round(1.52); // 2
+Math.ceil(1.12); // 2
+Math.floor(1.12); // 1
+Math.trunc(1.12); // 1
+Math.max(1, 2, 3, 4); // 4
+Math.min(1, 2, 3, 4); // 1
+Math.random(); // 0.xxx
+Math.pow(2, 3); // 8
+Math.sqrt(4); // 2
+
+Math.floor(-45.12); // -46
+Math.trunc(-45.12); // -45
